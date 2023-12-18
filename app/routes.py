8ebd8b264c780b8f1e328fe39c2a34d736f9400b
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-18 21:18:52 +0330

    Show Time

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,15 @@ from main import app
 
 @app.route('/')
 def index():
-    _tz = request.args.get('TZ', default='', type=str)
+    _tz = request.args.get('TZ', default='Asia/Tehran', type=str)
     time_now = TimeNow(_tz=_tz)
     date_now = DateNow()
-
+    
+    if not time_now:
+        return abort(404)
+    
     return render_template(
-        template_name_or_list='index.html')
+        template_name_or_list='index.html',
+        time=time_now)
 
 
