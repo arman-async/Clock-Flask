a77124e937605a191ade1919f09d959e864306f6
Alireza Mosaveyan <Armanparsi206@outlook.com>
2023-12-18 21:07:42 +0330

    Completing the function {TimeNow}

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,20 @@
-def TimeNow(_tz):
-    pass
+from datetime import datetime
+import pytz
+
+
+def TimeNow(_tz:str='Asia/Tehran')-> tuple[int, int, int] | None:
+    
+    try:
+        now = datetime.now(
+            tz=pytz.timezone(_tz))
+    except pytz.exceptions.UnknownTimeZoneError:
+        return None
+    
+    return now.hour, now.minute, now.second 
 
 def DateNow():
     pass
+
+
+if __name__ == '__main__':
+    print(TimeNow())
